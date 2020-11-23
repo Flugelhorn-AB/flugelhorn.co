9bdb1ac0af295a7b1030ff0b6dada59d884edfa8
strazan <sigge@labor.cool>
2020-11-23 17:36:50 -0600

    fix: query the right project
    fixed the bug that only queried Careerday UF. Now it works.

--- pages/work/[project].js
+++ pages/work/[project].js
@@ -1,41 +1,34 @@
-import Router from "next/router"
-import { useEffect } from "react"
-import { fetcher, url } from "../../utils/fetcher"
-import style from "./project.module.scss" 
+import Router from "next/router";
+import { useEffect } from "react";
+import { fetcher, url } from "../../utils/fetcher";
+import style from "./project.module.scss";
 
-const Project = ({details}) => {
-// const {project: {details}} = props
-useEffect(()=> {
-    Router.router.replace({pathname: details.name}, undefined,{ shallow: true})
-    },[])
-console.log(details)
+const Project = ({ details }) => {
+  // const {project: {details}} = props
+
+  console.log(details);
   return (
     <div className={style.Project}>
-        <h2>{details.name} 
-
-</h2>
+      <h2>{details.name}</h2>
     </div>
-  )
+  );
+};
+
+export async function getStaticPaths() {
+  const res = await fetcher(`${url}/projects`);
+  const paths = res.map((project) => {
+    let name = project.name;
+    name = name.replace(" ", "%20");
+    return { params: { project: name } };
+  });
+  return { paths, fallback: false };
 }
 
+export async function getStaticProps({ params }) {
+  console.log(params);
+  const res = await fetcher(`${url}/projects/?name=${params.project}`);
 
-
-export async function getStaticPaths(){
-    const res = await fetcher(`${url}/projects`)
-    const paths = res.map(project => {
-        let name = project.name
-        name = name.replace(" ", "%20")
-        return {params: {project:name}}
-    })
-    return {paths, fallback: false}
+  return { props: { details: res[0] } };
 }
 
-
-export async function getStaticProps({params}){
-    console.log(params)
-    const res = await fetcher(`${url}/projects/${params.project}`)
-
-    return {props:{ details: res}}
-}
-
-export default Project
+export default Project;

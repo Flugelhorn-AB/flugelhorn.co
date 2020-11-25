33e63b964adaaf0c5819568ea250b6576579776d
augusterixon <august.erixon@hyperisland.se>
2020-11-25 16:19:49 -0600

    feat: project page

--- components/ProjectHero.js
+++ components/ProjectHero.js
@@ -3,15 +3,31 @@ import { useEffect, useRef, useState } from 'react';
 
 const ProjectHero = ({img}) => {
      const [offset, setOffset] = useState();
+const overlay = useRef()
+
+
      const handleScroll = () => {
           setOffset(window.pageYOffset);
      };
+
+
+   
+
+    //  useEffect(() => {
+    //     if (window.pageYOffset > 700){
+    //         hideOverlay()
+    //     } else {showOverlay()}
+
+   
+    //  },[window.pageYOffset])
 
      useEffect(() => {
           document.addEventListener('scroll', handleScroll);
           return () => {
                document.removeEventListener('scroll', handleScroll);
           };
+
+          
      }, []);
 
      return (
@@ -20,11 +36,13 @@ const ProjectHero = ({img}) => {
                     top: `${offset * 0.6}px`,
                }}
                className={style.projectHero}
+
           >
 
                {img ? <img src={img} /> : ''}
                <div
-                    style={{ opacity: offset * 0.001 }}
+               ref={overlay}
+                    style={{ opacity: offset * 0.0016 }}
                     className={style.overlay}
                ></div>
           </div>
--- pages/work/[project].js
+++ pages/work/[project].js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Router from "next/router";
 import { useEffect } from "react";
 import Navigation from "../../components/Navigation";
@@ -20,34 +21,82 @@ const coverImg = `http://localhost:1337${details.cardImage.url}`;
       <h2 className={style.name}>{details.name}</h2>
       <h2 className={style.tags}>{details.tags}</h2>
       <h2 className={style.intro}>{details.intro}</h2>
+
+  
+
+      <Link href={details.link}>
+      <a  className={style.link}>View ↗</a>
+      </Link>
+
+
+      <div className={style.techStack}>
+      {details.techStack.map(technology => {
+          return <p>{technology.technology}</p>
+      })}
+</div>
       </div>
+
+
+        {/* <div className={style.fullImage}> */}
+        {/* <video  autoPlay muted loop> 
+        <source src={`http://localhost:1337${details.fullImage1[0].url}`} type="video/mp4" />
+        </video> */}
+        {/* </div> */}
+        
+<div className={style.descriptionContainer}>
+        <div className={style.description}>
+            <h1>About</h1>
+      <p>{details.description}</p>
+      </div></div>
+
+
+      {details.fullImage1[0].url ?
+      <div className={style.fullImage}>
+      <img src={`http://localhost:1337${details.fullImage1[0].url}`} />
+        </div>
+        : '' }
+
+
+{/* {details.imageRow[0].leftImg.ext === '.mov' ?
 
 <div className={style.leftImage}>
-{/* <img src={`http://localhost:1337${details.imageRow[0].leftImg.url}`} /> */}
+<video  autoPlay muted loop> 
+        <source src={`http://localhost:1337${details.imageRow[0].leftImg.url}`} type="video/mp4" />
+        </video> 
         </div>
+        :
+
+<div className={style.leftImage}>
+        <img src={`http://localhost:1337${details.imageRow[0].leftImg.url}`} />
+        </div>
+        }
+
+
+        {details.imageRow[0].rightImg.url ?
         <div className={style.rightImage}>
-        {/* <img src={`http://localhost:1337${details.imageRow[0].rightImg.url}`} /> */} */}
-        </div>
+        <img src={`http://localhost:1337${details.imageRow[0].rightImg.url}`} />
+        </div>  : ''}
+    
+        
+         */}
+ 
 
-        <div className={style.fullWidthImage}>
-        {/* <img src={`http://localhost:1337${details.fullWidthImage.url}`} /> */}
-        </div>
 
-        <div className={style.description}>
-      {/* <h2>{details.description}</h2> */}
-      </div>
+      {details.fullImage2.url ?
+      <div className={style.fullImage}>
+      <img src={`http://localhost:1337${details.fullImage2[0].url}`} />
+        </div>
+        : '' }
 
       <div className={style.process}>
       {details.stepProcess.map(step => {
-          return <div className={style.step}>{step.id}{step.step}</div>
+          return <div key={step.id} className={style.step}>{step.id}{step.step}</div>
       })}
       </div>
 
-<div className={style.techStack}>
-      {details.techStack.map(technology => {
-          return <h2>{technology.technology}</h2>
-      })}
-</div>
+
+
+
     </div>
     </div>
 

b68d209371631cc8c45bbf7cc25d6b18426172a1
augusterixon <august.erixon@hyperisland.se>
2020-11-26 15:02:41 -0600

    Fix: dja

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,6 @@
 import "../base/_normalize.scss";
-<<<<<<< Updated upstream
-
-=======
 import "../plugins/prismSolarLight.css";
-import 
->>>>>>> Stashed changes
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }

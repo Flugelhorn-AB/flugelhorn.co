4d1482104f4f3dea1f0b8133d8fb13493dafe92e
augusterixon <august.erixon@hyperisland.se>
2020-11-27 13:49:24 -0600

    feat: change project layout to <AboutBlock />

--- pages/work/[project].js
+++ pages/work/[project].js
@@ -5,6 +5,7 @@ import Navigation from "../../components/Navigation";
 import ProjectHero from "../../components/ProjectHero";
 import { fetcher, url } from "../../utils/fetcher";
 import style from "./project.module.scss";
+import AboutBlock from '../../components/AboutBlock.js';
 
 const Project = ({ details }) => {
   // const {project: {details}} = props
@@ -23,50 +24,43 @@ const coverImg = details.cardImage.url;
 
         <div className={style.project}>
         <div className={style.details}>
-            <h2 className={style.name}>{details.name}</h2>
-            <h2 className={style.tags}>{details.tags}</h2>
-            <h2 className={style.intro}>{details.intro}</h2>
-
-            {details.introImage.url ? 
+            <AboutBlock title={details.name}>
+            <p className={style.tags}>{details.tags}</p>
+            <p className={style.intro}>{details.intro}</p>
             <Link href={details.link}>
-                <a>
-                    <img className={style.introImage} src={details.introImage.url} />
-                </a>
+                <a>Visit ↗</a>
             </Link>
+            </AboutBlock>
+   
 
-: ''}
-      <Link href={details.link}>
-      <a  className={style.link}>View ↗</a>
-      </Link>
-
-
+<AboutBlock title="">
       <div className={style.stack}>
       {details.stack.map(technology => {
           return <p>{technology.technology}</p>
       })}
 </div>
+</AboutBlock>
       </div>
 
 
-      
+      <AboutBlock title="Description">
 <div className={style.descriptionContainer}>
         <div className={style.description}>
-            <h1>About</h1>
       <p>{details.description}</p>
       </div></div>
+      </AboutBlock>
 
 
 
  
-
+<AboutBlock title="Process">
       <div className={style.processContainer}> 
-          <h1>Project Overview</h1>
       <div className={style.process}>
       {details.stepProcess.map(step => {
           return <div key={step.id} className={style.step}><p>{step.id}. {step.step}</p></div>
       })}
       </div></div>
-
+      </AboutBlock>
 
 
 

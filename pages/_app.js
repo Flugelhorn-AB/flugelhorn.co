e0ee5076dceaf942a6c48c761752c25808511864
augusterixon <august.erixon@hyperisland.se>
2020-11-27 12:23:00 -0600

    feat: page transitions using framer-motion

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,20 @@
 import "../base/_normalize.scss";
 import "../plugins/prismSolarLight.css";
+import { motion, AnimatePresence } from 'framer-motion';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps, router }) {
+
+
+
+
+
+  return (<div>
+  <AnimatePresence >
+  <motion.div style={{position: 'absolute', width: '100%', height: '100%'}} key={router.route} transition={{duration: 0.5}}  exit={{backgroundColor:'white', opacity: 0}} initial={{opacity: 0}} animate={{opacity: 1}} >
+  <Component {...pageProps} />;
+  </motion.div>
+  </AnimatePresence>
+  </div>)
 }
 
 export default MyApp;

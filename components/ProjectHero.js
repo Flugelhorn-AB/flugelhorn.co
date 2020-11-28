5fc8e6ba04bde344a2572fd1c68d58b98cb9ce91
strazan <sigge@labor.cool>
2020-11-28 15:34:23 -0600

    feat: projectHero img style different depending on type

--- components/ProjectHero.js
+++ components/ProjectHero.js
@@ -1,52 +1,45 @@
-import style from './style/projectHero.module.scss';
-import { useEffect, useRef, useState } from 'react';
+import style from "./style/projectHero.module.scss";
+import { useEffect, useRef, useState } from "react";
 
-const ProjectHero = ({img}) => {
-     const [offset, setOffset] = useState();
-const overlay = useRef()
+const ProjectHero = ({ img, isWork }) => {
+  const [offset, setOffset] = useState();
+  const overlay = useRef();
 
+  const handleScroll = () => {
+    setOffset(window.pageYOffset);
+  };
 
-     const handleScroll = () => {
-          setOffset(window.pageYOffset);
-     };
+  //  useEffect(() => {
+  //     if (window.pageYOffset > 700){
+  //         hideOverlay()
+  //     } else {showOverlay()}
 
+  //  },[window.pageYOffset])
 
-   
+  useEffect(() => {
+    document.addEventListener("scroll", handleScroll);
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-    //  useEffect(() => {
-    //     if (window.pageYOffset > 700){
-    //         hideOverlay()
-    //     } else {showOverlay()}
-
-   
-    //  },[window.pageYOffset])
-
-     useEffect(() => {
-          document.addEventListener('scroll', handleScroll);
-          return () => {
-               document.removeEventListener('scroll', handleScroll);
-          };
-
-          
-     }, []);
-
-     return (
-          <div
-               style={{
-                    transform: `translateY(-${offset * 0.6}px)`,
-               }}
-               className={style.projectHero}
-
-          >
-
-               {img ? <img src={img} /> : ''}
-               <div
-               ref={overlay}
-                    style={{ opacity: offset * 0.0016 }}
-                    className={style.overlay}
-               ></div>
-          </div>
-     );
+  return (
+    <div
+      style={{
+        transform: `translateY(-${offset * 0.6}px)`,
+      }}
+      className={`${style.projectHero} ${
+        isWork ? style.workHero : style.aboutHero
+      }`}
+    >
+      {img ? <img src={img} /> : ""}
+      <div
+        ref={overlay}
+        style={{ opacity: offset * 0.0016 }}
+        className={style.overlay}
+      ></div>
+    </div>
+  );
 };
 
 export default ProjectHero;

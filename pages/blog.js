51166bc15adb20451bf877ff930771a81f2d7580
augusterixon <august.erixon@hyperisland.se>
2020-11-28 14:19:28 -0600

    feat: random hero background form array

--- pages/blog.js
+++ pages/blog.js
@@ -1,25 +1,53 @@
-import BlogCard from "../components/BlogCard";
-import BlogGallery from "../components/BlogGallery";
-import Footer from "../components/Footer";
-import Head from "../components/head";
-import Navigation from "../components/Navigation";
-import { url, fetcher } from "../utils/fetcher";
-import style from "./style/blog.module.scss";
+import { useState, useEffect } from 'react';
+import BlogCard from '../components/BlogCard';
+import BlogGallery from '../components/BlogGallery';
+import Footer from '../components/Footer';
+import Head from '../components/head';
+import Navigation from '../components/Navigation';
+import { url, fetcher } from '../utils/fetcher';
+import style from './style/blog.module.scss';
 
 const Blog = ({ articles }) => {
-  console.log(articles);
+     const [randomColor, setRandomColor] = useState('');
+     const colorArray = [
+          '#74b095',
+          '#d970e1',
+          '#6bd946',
+          '#e97996',
+          '#66e293',
+          '#ef7a46',
+          '#71dbd6',
+          '#dcde3f',
+          '#7ba8e0',
+          '#9ec647',
+          '#d1a0d2',
+          '#71b36c',
+          '#d7a07c',
+          '#cdd68f',
+          '#d1a63f',
+     ];
 
-  return (
-    <div className={style.blog}>
-      <Head
-        title="Blog | Flugelhorn Digital Web Agency"
-        description="
+     useEffect(() => {
+          setRandomColor(
+               colorArray[Math.floor(Math.random() * colorArray.length)]
+          );
+     }, []);
+
+     return (
+          <div className={style.blog}>
+               <Head
+                    title="Blog | Flugelhorn Digital Web Agency"
+                    description="
         Read our latest articles about web, design and travel. Learn the latest within web development and graphic design."
-        image="/flugel-fam.png"
-      />
-      <Navigation />
-      <BlogGallery articles={articles} />
-      {/* <div className={style.posts}>
+                    image="/flugel-fam.png"
+               />
+               <div
+                    style={{ backgroundColor: randomColor }}
+                    className={style.navigationUnderlay}
+               />
+               <Navigation />
+               <BlogGallery articles={articles} />
+               {/* <div className={style.posts}>
         <div className={style.col}>
           {posts.map((post, i) => {
             return <BlogCard details={post} key={i} />;
@@ -31,14 +59,14 @@ const Blog = ({ articles }) => {
           })}
         </div>
       </div> */}
-<Footer />
-    </div>
-  );
+               <Footer />
+          </div>
+     );
 };
 export const getStaticProps = async () => {
-  const articles = await fetcher(`${url}/articles`);
-  console.log(articles);
-  return { props: { articles } };
+     const articles = await fetcher(`${url}/articles`);
+     console.log(articles);
+     return { props: { articles } };
 };
 
 export default Blog;

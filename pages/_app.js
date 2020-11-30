baf0b07d6282618f0ec34af886bfd6560c89b777
augusterixon <august.erixon@hyperisland.se>
2020-11-30 10:59:30 -0600

    fix: hero link now hoverable
    problem: hero had z-index: -10 -> changed to z-index: 0

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,24 @@
-import "../base/_normalize.scss";
-import "../plugins/prismSolarLight.css";
+import '../base/_normalize.scss';
+import '../plugins/prismSolarLight.css';
 import { motion, AnimatePresence } from 'framer-motion';
 
 function MyApp({ Component, pageProps, router }) {
-
-
-
-
-
-  return (<div>
-  <AnimatePresence >
-  <motion.div style={{position: 'absolute', width: '100%', height: '100%'}} key={router.route} transition={{duration: 0.5}}  exit={{backgroundColor:'white', opacity: 0}} initial={{opacity: 0}} animate={{opacity: 1}} >
-  <Component {...pageProps} />
-  </motion.div>
-  </AnimatePresence>
-  </div>)
+     return (
+          <div>
+               <AnimatePresence initial={false} exitBeforeEnter>
+                    <motion.div
+                         style={{ zIndex: '0' }}
+                         key={router.route}
+                         transition={{ duration: 0.5 }}
+                         exit={{ backgroundColor: 'white', opacity: 0 }}
+                         initial={{ opacity: 0 }}
+                         animate={{ opacity: 1 }}
+                    >
+                         <Component {...pageProps} />
+                    </motion.div>
+               </AnimatePresence>
+          </div>
+     );
 }
 
 export default MyApp;
